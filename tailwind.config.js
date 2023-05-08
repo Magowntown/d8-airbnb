207e19f7b7c2a6dbcbc752cda29e7a23f3760539
Magowntown <amagown@gmail.com>
2023-05-08 00:45:17 -0400

    Abandoned DaisyUI, too complicated. Added Tailwind search bars, search button, bottom Banner, and moderate media responsiveness. Minor Corrections still needed.

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,16 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
 }
 
